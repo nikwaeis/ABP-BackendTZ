93ea99fc0410cb8d473b8fa1bb58b6265d08b4ee
Mykyta <237848768+nikwaeis@users.noreply.github.com>
2026-08-27 13:04:19 +0200

    initialize PostgreSQL configuration, ignoring IDE files

--- src/backend/ReserveSpace.Api/Program.cs
+++ src/backend/ReserveSpace.Api/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ReserveSpace.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
